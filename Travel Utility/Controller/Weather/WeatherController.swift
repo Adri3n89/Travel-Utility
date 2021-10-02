cd2dfbdbd84237ce10c1c0823ac54ffd266d8e3d
Adrien <adrien@perea.be>
2021-10-02 22:24:54 +0200

    codage Weather

--- Travel Utility/Controller/Weather/WeatherController.swift
+++ Travel Utility/Controller/Weather/WeatherController.swift
@@ -16,21 +16,78 @@ class WeatherController: UIViewController {
     @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var unitLabel: UILabel!
 
+    // MARK: - Private Variables
+
+    private var weatherInfo = WeatherInfo()
+    private var language = "fr"
+    private var unit = "metric"
+
+    // MARK: - ViewDidLoad
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addTap()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Private Methods
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    private func addTap() {
+        // add tapGesture to UIImageView
+        let tapParis = UITapGestureRecognizer(target: self, action: #selector(showParis))
+        parisImageView.addGestureRecognizer(tapParis)
+        let tapNewYork = UITapGestureRecognizer(target: self, action: #selector(showNewYork))
+        newyorkImageView.addGestureRecognizer(tapNewYork)
+    }
+
+    // set the action to do when tap on Paris
+    @objc private func showParis() {
+        setWeatherAndPerformSegue(city: "Paris")
+    }
+
+    // set the action to do when tap on New York
+    @objc private func showNewYork() {
+        setWeatherAndPerformSegue(city: "New+york")
+    }
+
+    // setup function to do when tap
+    private func setWeatherAndPerformSegue(city: String) {
+        weatherInfo.city = city
+        weatherInfo.system = unit
+        weatherInfo.unit = unitLabel.text!
+        weatherInfo.language = languageLabel.text!
+        performSegue(withIdentifier: "Detail", sender: weatherInfo)
+    }
+
+    // MARK: - Methods
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Detail" {
+            if let newController = segue.destination as? WeatherDetailController {
+                newController.weatherInfo = sender as? WeatherInfo
+            }
+        }
     }
-    */
+
+    // MARK: - IBActions
+    // change setting by checking the tag of the sender to change the right setting ( language or unit )
+    @IBAction func changeSettingSwitched(_ sender: UISwitch) {
+        if sender.tag == 0 {
+            if sender.isOn {
+                languageLabel.text = "FR"
+                language = "fr"
+            } else {
+                languageLabel.text = "EN"
+                language = "en"
+            }
+        } else {
+            if sender.isOn {
+                unitLabel.text = "°C"
+                unit = "metric"
+            } else {
+                unitLabel.text = "°F"
+                unit = "imperial"
+            }
+        }
+    }
 
 }
--- Travel Utility/Controller/Weather/WeatherDetailController.swift
+++ Travel Utility/Controller/Weather/WeatherDetailController.swift
@@ -16,21 +16,80 @@ class WeatherDetailController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
 
+    // MARK: - Variables
+
+    var weatherInfo: WeatherInfo?
+    var weather5Days: WeatherForFiveDaysInfo?
+    let weatherService = WeatherService()
+
+    // MARK: - ViewDidLoad
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
+        tableView.reloadData()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - ViewWillAppear
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        weatherService.getWeather(city: weatherInfo!.city, unit: weatherInfo!.system, language: weatherInfo!.language) { (error, weather) in
+            if let error = error {
+                self.presentAlert(message: error.localizedDescription)
+            } else if let weather = weather {
+                self.temperatureLabel.text = "\(weather.main.temp) \(String(describing: self.weatherInfo!.unit))"
+                self.descriptionLabel.text = weather.weather[0].description
+                self.weatherService.getIcon(icon: weather.weather[0].icon) { (error, icon) in
+                    if let error = error {
+                        self.presentAlert(message: error.localizedDescription)
+                    } else if let icon = UIImage(data: icon!) {
+                        self.iconImageView.image = icon
+                        self.weatherService.getWeatherForFiveDays(city: self.weatherInfo!.city, unit: self.weatherInfo!.system) {  (error, weather) in
+                            if let error = error {
+                                self.presentAlert(message: error.localizedDescription)
+                            } else if let weather = weather {
+                                self.weather5Days = weather
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    */
 
+    @IBAction func returnPushed(sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Extension TableView
+
+extension WeatherDetailController: UITableViewDelegate, UITableViewDataSource {
+
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.layer.borderWidth = 2
+        tableView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tableView.layer.cornerRadius = 25
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell") as? WeatherCell {
+            if weather5Days != nil {
+                cell.dateLabel.text = weather5Days!.weather[indexPath.row].date
+                cell.tempLabel.text = String(weather5Days!.weather[indexPath.row].temp) + weatherInfo!.unit
+                cell.iconImageView.image = UIImage(named: weather5Days!.weather[indexPath.row].icon)
+            }
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weather5Days?.weather.count ?? 0
+    }
 }
