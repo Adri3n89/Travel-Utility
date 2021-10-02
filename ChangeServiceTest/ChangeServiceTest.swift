cb6019286ee4eac2fa0ba04bc91720a014192acc
Adrien <adrien@perea.be>
2021-10-02 22:34:57 +0200

    Test Change Service

---
+++ ChangeServiceTest/ChangeServiceTest.swift
@@ -0,0 +1,272 @@
+//
+//  ChangeServiceTest.swift
+//  ChangeServiceTest
+//
+//  Created by Adrien PEREA on 02/10/2021.
+//
+
+@testable import Travel_Utility
+import XCTest
+
+class ChangeServiceTest: XCTestCase {
+
+    // MARK: - TEST GetChange
+
+    func testGetChangeGivenDataAndGoodResponseAndNoError() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseOK
+        let jsonData = FakeResponseData().changeCorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getChange { error, devise in
+            XCTAssertNil(error)
+            XCTAssertNotNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetChangeGivenIncorrectDataAndGoodResponseAndNoError() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseOK
+        let jsonData = FakeResponseData().changeIncorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getChange { error, devise in
+            XCTAssertEqual(error, .undecodableData)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetChangeGivenErrorAndBadResponse() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseKO
+        let error = FakeResponseData().error
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (nil, response, error)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getChange { error, devise in
+            XCTAssertEqual(error, .noData)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetChangeGivenBadResponse() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseKO
+        let jsonData = FakeResponseData().changeCorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getChange { error, devise in
+            XCTAssertEqual(error, .badResponse)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    // MARK: - TEST GetSymbols
+
+    func testGetSymbolsGivenDataAndGoodResponseAndNoError() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseOK
+        let jsonData = FakeResponseData().symbolsCorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getSymbols { error, devise in
+            XCTAssertNil(error)
+            XCTAssertNotNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetSymbolsGivenIncorrectDataAndGoodResponseAndNoError() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseOK
+        let jsonData = FakeResponseData().changeIncorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getSymbols { error, devise in
+            XCTAssertEqual(error, .undecodableData)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetSymbolsGivenErrorAndBadResponse() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseKO
+        let error = FakeResponseData().error
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (nil, response, error)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getSymbols { error, devise in
+            XCTAssertEqual(error, .noData)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    func testGetSymbolsGivenBadResponse() {
+        //given
+        ChangeURLProtocol.loadingHandler = nil
+        let response = FakeResponseData().reponseKO
+        let jsonData = FakeResponseData().changeCorrectData
+
+        ChangeURLProtocol.loadingHandler = { request in
+            return (jsonData, response, nil)
+        }
+
+        let expectation = XCTestExpectation(description: "changing queue")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        changeService.getSymbols { error, devise in
+            XCTAssertEqual(error, .badResponse)
+            XCTAssertNil(devise)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+    }
+
+    // MARK: - TEST ConvertDevise
+
+    func testGiven2NumbersAndFormatThem() {
+        // given
+        let number = 12.2
+        let input = 2.3493828
+        let output = 3.1
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [ChangeURLProtocol.self]
+        let changeService = ChangeService(urlSession: URLSession(configuration: configuration))
+
+        let result = changeService.convertDevise(number: number, input: input, output: output)
+        XCTAssertEqual(result, "16.0978")
+    }
+
+}
+
+// fake urlProtocol
+final class ChangeURLProtocol: URLProtocol {
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    static var loadingHandler: ((URLRequest) -> (Data?, HTTPURLResponse, Error?))?
+
+    override func startLoading() {
+        guard let handler = ChangeURLProtocol.loadingHandler else {
+            XCTFail("Loading handler is not set.")
+            return
+        }
+        let (data, response, error) = handler(request)
+        if let data = data {
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        else {
+            client?.urlProtocol(self, didFailWithError: error!)
+        }
+    }
+
+    override func stopLoading() {}
+}
---
+++ ChangeServiceTest/FakeResponseData.swift
@@ -0,0 +1,35 @@
+//
+//  FakeResponseData.swift
+//  ChangeServiceTest
+//
+//  Created by Adrien PEREA on 02/10/2021.
+//
+
+import Foundation
+
+class FakeResponseData {
+
+    let reponseOK = HTTPURLResponse(url: URL(string: "http://validateproject9.fr")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+
+    let reponseKO = HTTPURLResponse(url: URL(string: "http://validateproject9.fr")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
+
+    class ChangeError: Error {}
+    let error = ChangeError()
+
+    var changeCorrectData: Data {
+        let bundle = Bundle(for: FakeResponseData.self)
+        let url = bundle.url(forResource: "Change", withExtension: "json")
+        let data = try! Data(contentsOf: url!)
+        return data
+    }
+
+    var symbolsCorrectData: Data {
+        let bundle = Bundle(for: FakeResponseData.self)
+        let url = bundle.url(forResource: "Symbols", withExtension: "json")
+        let data = try! Data(contentsOf: url!)
+        return data
+    }
+
+    let changeIncorrectData = "erreur".data(using: .utf8)!
+
+}
